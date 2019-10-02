7a22530f1129117b7d93008420af909577d5eba6
JonnathanAndres <andres.652@hotmail.com>
2019-10-02 07:50:39 -0500

    Use IHostingEnvironment in Configure

--- Practica2/Startup.cs
+++ Practica2/Startup.cs
@@ -15,20 +15,32 @@ namespace Practica2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var msg = $"Ambiente actual de la app: {env.EnvironmentName}";
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
+                app.Run(async (context) =>
+                {
+                    msg = "El ambiente es de desarrollo";
+                    await context.Response.WriteAsync(msg);
+                });
+            }
+            else
+            {
+                app.Run(async (context) =>
+                {
+                    msg = $"En almbiente actual no es de desarrollo. Es de {env.EnvironmentName}";
+                    await context.Response.WriteAsync(msg);
+                });
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            
         }
     }
 }
