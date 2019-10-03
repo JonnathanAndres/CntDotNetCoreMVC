38dc0bd7ba82963f234f8d13565b21b285b943ef
JonnathanAndres <andres.652@hotmail.com>
2019-10-03 18:22:24 -0500

    Middleware order

--- Practica2/Startup.cs
+++ Practica2/Startup.cs
@@ -19,28 +19,45 @@ namespace Practica2
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            var msg = $"Ambiente actual de la app: {env.EnvironmentName}";
-            if (env.IsDevelopment())
-            {
-                //app.UseDeveloperExceptionPage();
-                app.Run(async (context) =>
-                {
-                    msg = "El ambiente es de desarrollo";
-                    await context.Response.WriteAsync(msg);
-                });
-            }
-            else
-            {
-                app.Run(async (context) =>
-                {
-                    msg = $"En almbiente actual no es de desarrollo. Es de {env.EnvironmentName}";
-                    await context.Response.WriteAsync(msg);
-                });
-            }
+            // primer middleware de la aplicación:
 
-            
+            // Hello world middleware
+            app.Use(async (ctx, next) =>
+            {
+                if (ctx.Request.Path == "/hello-world")
+                {
+                    // Procesa la petición y no permite la ejecución de middlewares posteriores
+                    await ctx.Response.WriteAsync("Hello, world!");
+                }
+                else
+                {
+                    // Pasa el control al siguiente middleware
+                    await next();
+                }
+            });
+
+            //segundo middleware:
+            app.Use(async (ctx, next) =>
+            {
+                if (ctx.Request.Path.ToString().StartsWith("/hello"))
+                {
+                    // Procesa la petición y no permite la ejecución de otros middlewares
+                    await ctx.Response.WriteAsync("Hello, user!");
+                }
+                else
+                {
+                    // Pasa la petición al siguiente middleware
+                    await next();
+                }
+            });
+
+            // Request Info middleware
+            app.Run(async ctx =>
+            {
+                await ctx.Response.WriteAsync($"Path requested: {ctx.Request.Path}");
+            });
         }
     }
 }
